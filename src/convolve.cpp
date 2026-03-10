e4d9b8019b72b5ee525068917ed1c4d19ebf4a24
David Dye <dwdjr@outlook.com>
2026-03-10 19:40:33 -0400

    switched default version to be the handmade fast convolution instead of the FFTW implementation.

--- src/convolve.cpp
+++ src/convolve.cpp
@@ -9,30 +9,7 @@
 
 Convolver::Convolver() {}
 
-Convolver::~Convolver() {
-    if (plan_fwd_1) {
-        fftwf_destroy_plan(plan_fwd_1);
-    }
-    if (plan_fwd_2) {
-        fftwf_destroy_plan(plan_fwd_2);
-    }
-    if (plan_inv) {
-        fftwf_destroy_plan(plan_inv);
-    }
-
-    if (in1) {
-        fftwf_free(in1);
-    }
-    if (in2) {
-        fftwf_free(in2);
-    }
-    if (freq1) {
-        fftwf_free(freq1);
-    }
-    if (freq2) {
-        fftwf_free(freq2);
-    }
-}
+Convolver::~Convolver() {}
 
 std::vector<Convolver::Complex_T>& Convolver::convolve(
     const Complex_T* const z1,
@@ -51,49 +28,6 @@ std::vector<Convolver::Complex_T>& Convolver::convolve(
         return conv_out;
     }
 
-    uint64_t out_size = n_z1 + n_z2 - 1;
-    plan_convolution(n_z1, n_z2);
-
-    memcpy((void*)in1, z1, sizeof(Complex_T) * n_z1);
-    memcpy((void*)in2, z2, sizeof(Complex_T) * n_z2);
-    memset((void*)(in1 + n_z1), 0, sizeof(Complex_T) * (planned_size - n_z1));
-    memset((void*)(in2 + n_z2), 0, sizeof(Complex_T) * (planned_size - n_z2));
-
-    fftwf_execute(plan_fwd_1);
-    fftwf_execute(plan_fwd_2);
-
-    for (uint64_t i = 0; i < planned_size; ++i) {
-        float a = freq1[i][0];
-        float b = freq1[i][1];
-        float c = freq2[i][0];
-        float d = freq2[i][1];
-
-        freq1[i][0] = a*c - b*d;
-        freq1[i][1] = a*d + b*c;
-    }
-
-    fftwf_execute(plan_inv);
-
-    float scale = 1.0f / planned_size;
-    conv_out.resize(out_size);
-
-    for (uint64_t i = 0; i < out_size; ++i) {
-        conv_out[i] = Complex_T(
-            in1[i][0] * scale,
-            in1[i][1] * scale
-        );
-    }
-
-    return conv_out;
-
-}
-
-void Convolver::plan_convolution(uint64_t n_z1, uint64_t n_z2) {
-    if (n_z1 == 0 || n_z2 == 0) {
-        std::cerr << "Error: plan_convolution: data buffers must contain elements" << std::endl;
-        return;
-    }
-
     uint64_t out_size_min = n_z1 + n_z2 - 1;
     uint64_t out_size_true = out_size_min;
     uint8_t n_bits = ui_log2_ui64(out_size_min);
@@ -101,51 +35,35 @@ void Convolver::plan_convolution(uint64_t n_z1, uint64_t n_z2) {
         ++n_bits;
         out_size_true = UINT64_C(1) << n_bits; // next power of 2 above the out_size_min
     }
+    conv_out.resize(out_size_true);  // the full size of the convolution
+    conv_in_1.resize(out_size_true);
+    conv_in_2.resize(out_size_true);
 
-    if (out_size_true == planned_size) {
-        return;
+    memcpy((void*)(conv_in_1.data()), z1, sizeof(Complex_T) * n_z1);
+    memcpy((void*)(conv_in_2.data()), z2, sizeof(Complex_T) * n_z2);
+    memset((void*)(conv_in_1.data() + n_z1), 0, sizeof(Complex_T) * (conv_in_1.size() - n_z1));
+    memset((void*)(conv_in_2.data() + n_z2), 0, sizeof(Complex_T) * (conv_in_2.size() - n_z2));
+
+    if (!fft<false>(conv_in_1.data(), out_size_true)) {
+        std::cerr << "Error: convolve: FFT error." << std::endl;
+        conv_out.resize(0);
+        return conv_out;
+    }
+    if (!fft<false>(conv_in_2.data(), out_size_true)) {
+        std::cerr << "Error: convolve: FFT error." << std::endl;
+        conv_out.resize(0);
+        return conv_out;
+    }
+    for (uint64_t i = 0; i < out_size_true; ++i) {
+        conv_out[i] = conv_in_1[i] * conv_in_2[i];
+    }
+    if (!ifft<false>(conv_out.data(), out_size_true)) {
+        std::cerr << "Error: convolve: IFFT error." << std::endl;
+        conv_out.resize(0);
+        return conv_out;
     }
 
-    // Destroy old plans/buffers if they exist
-    if (plan_fwd_1) {
-        fftwf_destroy_plan(plan_fwd_1);
-    }
-    if (plan_fwd_2) {
-        fftwf_destroy_plan(plan_fwd_2);
-    }
-    if (plan_inv) {
-        fftwf_destroy_plan(plan_inv);
-    }
+    conv_out.resize(n_z1 + n_z2 - 1); // the useful size of the convolution
+    return conv_out;
 
-    if (in1) {
-        fftwf_free(in1);
-    }
-    if (in2) {
-        fftwf_free(in2);
-    }
-    if (freq1) {
-        fftwf_free(freq1);
-    }
-    if (freq2) {
-        fftwf_free(freq2);
-    }
-
-    planned_size = out_size_true;
-
-    in1   = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * planned_size);
-    in2   = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * planned_size);
-    freq1 = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * planned_size);
-    freq2 = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * planned_size);
-
-    plan_fwd_1 = fftwf_plan_dft_1d(
-        planned_size, in1, freq1, FFTW_FORWARD, FFTW_ESTIMATE
-    );
-
-    plan_fwd_2 = fftwf_plan_dft_1d(
-        planned_size, in2, freq2, FFTW_FORWARD, FFTW_ESTIMATE
-    );
-
-    plan_inv = fftwf_plan_dft_1d(
-        planned_size, freq1, in1, FFTW_BACKWARD, FFTW_ESTIMATE
-    );
 }
